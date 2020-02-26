6e1e54bd1089a32f587c231e331693cecf717b2a
luisAsaenz <59874758+luisAsaenz@users.noreply.github.com>
2020-02-26 08:44:10 -0500

    Add files via upload

---
+++ AreaOfTriangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AreaOfTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Side A is ");
+            int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+         
+            Console.Write("Side B is ");
+            int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+           
+            Console.Write("Side C is ");
+            int c = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+            
+            Console.Write("Side P is ");
+            int p = (a + b + c) / 2;
+            Console.WriteLine(p);
+
+            if (p > 0)
+            {
+                double area = Math.Sqrt(p * ((p - a) * (p - b) * (p - c)));
+                Console.WriteLine("Area of the triangle is " + area);
+            }
+
+        }
+    }
+}
